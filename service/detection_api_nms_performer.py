c2c85804bd4899aaf6b4808c97a3c2d89edcce6d
Tetro <itsofirk@gmail.com>
2020-10-18 09:44:41 +0300

    detection_api_nms_performer.py - under construction

--- service/detection_api_nms_performer.py
+++ service/detection_api_nms_performer.py
@@ -1,12 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from object_detection.core.post_processing import multiclass_non_max_suppression
+from tensorflow.python.ops.image_ops_impl import non_max_suppression_with_scores as multiclass_non_max_suppression
 from tensorflow.core.protobuf.config_pb2 import ConfigProto
 from tensorflow.python.client.session import Session
 
 from .abstracts import NmsPerformerBase
-
-# from .object_detection.core.post_processing import multiclass_non_max_suppression
 
 SCORE_THRESH = 0.001
 
@@ -21,7 +20,6 @@ class DetectionApiNmsPerformer(NmsPerformerBase):
         """
         create tensorflow session
         """
-
         self.sess = Session(config=self.config)
 
     def nms_single_class(self, boxes, scores, input_metadata):
@@ -33,8 +31,8 @@ class DetectionApiNmsPerformer(NmsPerformerBase):
         max_output_size = boxes.shape[0]
 
         # expand to adapt to expected input dimensions
-        boxes = boxes[:, np.newaxis, :]
-        scores = scores[:, np.newaxis]
+        # boxes = boxes[:, np.newaxis, :]
+        # scores = scores[:, np.newaxis]
 
         # convert to tensors
         boxes = tf.constant(boxes, tf.float32)
@@ -42,8 +40,11 @@ class DetectionApiNmsPerformer(NmsPerformerBase):
 
         # create nms graph node
         nms_node = multiclass_non_max_suppression(
-            boxes, scores, score_thresh, iou_thresh, max_output_size)
-
+            # object_detection
+            # boxes, scores, score_thresh, iou_thresh, max_output_size
+            # tensorflow
+            boxes, scores, max_output_size, iou_thresh, score_thresh
+        )
         # run nms evaluation
         self.sess.run(nms_node.get())
         nms_boxes = nms_node.data['boxes'].eval(session=self.sess)
