53cb0876ad6b51217a805ba3314716ead69146be
Tetro <itsofirk@gmail.com>
2020-10-21 16:39:07 +0300

    Removed unnecessary comments

--- logic/cpu_nms.py
+++ logic/cpu_nms.py
@@ -8,19 +8,14 @@ def non_maximum_suppression(boxes: np.ndarray, scores: np.ndarray, input_metadat
     iou_thresh = input_metadata.get('nmsThresh', 0.6)
     max_output_size = boxes.shape[0]
 
-    D = np.empty((0, 4))  # init results list
+    D = np.empty((0, 4))
     while len(boxes) > 0:
-        # find the highest score index
         m = scores.argmax()
-        # assign the corresponding box
         M = Rectangle.from_coords(*boxes[m])
-        # add to final list and remove from candidates
         boxes = np.delete(boxes, m, 0)
         D = np.vstack([D, M])
-        # find similar boxes with overlap > threshold
         for i, (box, score) in enumerate(zip(boxes, scores)):
             if M.iou(Rectangle.from_coords(*box)) >= iou_thresh:
-                # if so, disqualify
                 boxes = np.delete(boxes, i, 0)
                 scores = np.delete(scores, i, 0)
 
@@ -42,16 +37,13 @@ def multiclass_non_maximum_suppression(boxes, scores, classes, input_metadata):
     api_utils.check_correspondence(boxes, scores, classes)
     if len(scores) == 0:
         return boxes, scores, classes
-    # Find the set of `classes` for this input
     unique_classes = np.unique(classes)
 
-    # Create lists for each class output
     nms_boxes_list = np.empty((0, 4))
     nms_scores_list = np.empty((0,))
     nms_classes_list = np.empty((0,))
 
     for i, class_name in enumerate(unique_classes):
-        # Find the input for each class
         class_indices = classes == class_name  # numpy syntax
         _boxes = boxes[class_indices]
         _scores = scores[class_indices]
@@ -64,7 +56,6 @@ def multiclass_non_maximum_suppression(boxes, scores, classes, input_metadata):
         nms_boxes_list = np.vstack((nms_boxes_list, _boxes))
         nms_scores_list = np.hstack((nms_scores_list, _scores))
         nms_classes_list = np.hstack((nms_classes_list, _classes))
-    # end-loop
 
     api_utils.check_correspondence(nms_boxes_list, nms_scores_list, nms_classes_list)
     return nms_boxes_list, nms_scores_list, nms_classes_list
